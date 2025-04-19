4c98c1d304177799864ebe4097e44b01dab4a0c9
Benjamin Gudehus <hastebrot@gmail.com>
2025-04-20 01:08:45 +0200

    feat: Fluent react added.

--- sources/playground-app/src/pages/IndexPage.tsx
+++ sources/playground-app/src/pages/IndexPage.tsx
@@ -1,12 +1,49 @@
+import {
+  Button,
+  FluentProvider,
+  makeStyles,
+  tokens,
+  webDarkTheme,
+} from "@fluentui/react-components";
+import { bundleIcon, SaveFilled, SaveRegular } from "@fluentui/react-icons";
 import { classNames } from "../helpers/clsx";
 
 export const IndexPage = () => {
   return (
-    <div className="grid min-h-dvh">
-      <div className={classNames("m-4 grid grid-rows-[auto_1fr]")}>
-        <section className="px-4">heading</section>
-        <section className="px-4 pt-4">content</section>
+    <FluentProvider theme={webDarkTheme}>
+      <div className="grid min-h-dvh">
+        <div className={classNames("my-4 grid grid-rows-[auto_1fr]")}>
+          <section className="px-4">heading</section>
+          <section className="px-4 pt-4">
+            <div>content</div>
+            <Content />
+          </section>
+        </div>
       </div>
+    </FluentProvider>
+  );
+};
+
+const SaveIcon = bundleIcon(SaveFilled, SaveRegular);
+
+const useStyles = makeStyles({
+  wrapper: {
+    display: "flex",
+    columnGap: tokens.spacingHorizontalS,
+  },
+});
+
+const Content = () => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.wrapper}>
+      <Button appearance="secondary" icon={<SaveIcon />}>
+        Text
+      </Button>
+      <Button appearance="primary" icon={<SaveIcon />}>
+        Text
+      </Button>
     </div>
   );
 };
